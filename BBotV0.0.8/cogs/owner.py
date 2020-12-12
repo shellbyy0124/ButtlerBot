a8e5e631d2a6a7fe74ae9a22356198c121c47716
shellbyy0124 <lisenbyscreation@outlook.com>
2020-12-12 13:19:46 -0500

    update for Kastien

---
+++ BBotV0.0.8/cogs/owner.py
@@ -0,0 +1,51 @@
+import datetime
+import discord
+import random
+import json
+
+from discord.ext import commands
+from discord.ext.commands import Cog
+
+with open('./master.json', 'r', encoding='utf-8-sig') as f:
+    data = json.load(f)
+
+mekasu = data["mekasu"]
+kastien = data["kastien"]
+staff_commands = data["staff_commands"]
+
+class OwnerCommands(commands.Cog):
+
+    def __init__(self, bot):
+
+        self.bot=bot
+
+    @commands.command(aliases=['mekasu'])
+    @commands.has_any_role('Owner')
+    async def ownerhelpmenu(self, ctx):
+
+        owner1 = discord.Embed(color=random.randint(0, 0xFFFFFF), title=f"Welcome Mistress {self.bot.get_user(mekasu).name}. How may I assist you today?")
+        owner1.add_field(name="Are you wanting to display information about me?", value="if so then please use `>binfo`")
+        owner1.add_field(name="__", value="__")
+        owner1.timestamp = datetime.datetime.utcnow()
+        owner1.set_thumbnail(url=self.bot.get_user(mekasu))
+
+        channel = self.bot.get_channel(staff_commands)
+        await channel.send(embed=owner1)
+
+
+    @commands.command(aliases=['kastien'])
+    @commands.has_role('Head Dev')
+    async def help2(self, ctx):
+
+        owner2 = discord.Embed(color=random.randint(0, 0xFFFFFF), title=f"Welcome Mister {self.bot.get_user(kastien).name}. How may I assist you today?")
+        owner2.add_field(name="Are you wanting to display information about me?", value="if so then please use `>binfo`")
+        owner2.add_field(name="__", value="__")
+        owner2.timestamp = datetime.datetime.utcnow()
+        owner2.set_thumbnail(url=self.bot.get_user(kastien))
+
+        channel = self.bot.get_channel(staff_commands)
+        await channel.send(embed=owner2)
+
+        
+def setup(bot):
+    bot.add_cog(OwnerCommands(bot))
---
+++ BBotV0.0.8/cogs/taskloops.py
@@ -0,0 +1,30 @@
+import discord
+import asyncio
+
+from discord.ext import commands, tasks
+from discord.ext.commands import Cog
+
+class TaskLoops(commands.Cog):
+
+    def __init__(self, bot):
+
+        self.bot=bot
+        self.changepresence.start()
+
+    @tasks.loop(seconds=10)
+    async def changepresence(self):
+        """Will loop every 60 seconds and change the bots presence"""
+        await self.bot.wait_until_ready()
+        await self.bot.change_presence(activity=discord.Game(name='if you cant remember, type >bhelp!'))
+        await asyncio.sleep(10)
+        await self.bot.change_presence(activity=discord.Game(name=f'Member count: TBD'))
+        await asyncio.sleep(10)
+        await self.bot.change_presence(activity=discord.Game(name='Released: 12/02/2020 By: Shellbyy'))
+        await asyncio.sleep(10)
+        await self.bot.change_presence(activity=discord.Game(name='Team Members: Mekasu, Kastien-Dev, KortaPo'))
+        await asyncio.sleep(10)
+        await self.bot.change_presence(activity=discord.Game(name='buttlerprefix will tell you the bots prefix!'))
+        await asyncio.sleep(10)
+
+def setup(bot):
+    bot.add_cog(TaskLoops(bot))
